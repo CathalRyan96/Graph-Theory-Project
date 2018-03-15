1ff2e1cbf781bfb19360735f9fca2cf107872cd5
CathalRyan96 <cathalryan48@gmail.com>
2018-03-15 11:27:32 +0000

    Added switch statement

--- shunt.go
+++ shunt.go
@@ -11,13 +11,37 @@ func intopost(infix string) string {
 	s := []rune{}
 
 	//For loop to loop over input
-	for _, r := range infix{
-		
-	}
+	for _, r := range infix {
+		switch {
+		case r == '(':
+			s = append(s, r)
+
+		case r == ')':
+			for s[len(s)-1] != '(' {
+				pofix = append(pofix, s[len(s)-1])
+				s = s[:len(s)-1]
+			}
+			s = s[:len(s)-1]
+
+		case specials[r] > 0:
+			for len(s) > 0 && specials[r] <= specials[s[len(s)-1]] {
+				pofix = append(pofix, s[len(s)-1])
+				s = s[:len(s)-1]
+			}
+			s = append(s, r)
+		default:
+			pofix = append(pofix, r)
+
+		}
 
 	}
 
+	for len(s) > 0 {
+		pofix = append(pofix, s[len(s)-1])
+		s = s[:len(s)-1]
+	}
 	return string(pofix)
+
 }
 
 func main() {
