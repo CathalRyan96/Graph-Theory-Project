1c44bccc806b02bafe5fd6dd8c5d2cd535abcbfa
CathalRyan96 <cathalryan48@gmail.com>
2018-03-26 11:37:03 +0100

    added state struct and nfa struct

--- nfa.go
+++ nfa.go
@@ -1,14 +1,25 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
-func poregtonfa(pofix string){
+type state struct {
+	symbol rune
+	edge1  *state
+	edge2  *state
+}
+
+type nfa struct {
+	initial *state
+	accept  *state
+}
+
+func poregtonfa(pofix string) *nfa {
 
 }
 
-func main(){
+func main() {
 	nfa := poregtonfa("ab.c*|")
 	fmt.Println("nfa")
 }
